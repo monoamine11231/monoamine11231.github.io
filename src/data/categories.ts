23b0628a4b19c5030c03547fa0bf5a5fb13cf74c
Ioannis <68696386+monoamine11231@users.noreply.github.com>
2025-01-02 11:40:26 +0100

    chore(core): Add appropriate categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,7 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Programming',
+	'Algorithms',
+	'Cybersecurity',
+	'Machine Learning',
 ] as const
