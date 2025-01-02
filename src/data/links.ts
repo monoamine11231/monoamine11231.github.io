3869d1840e28d628eee5dcd496c34173f2a454df
Ioannis <68696386+monoamine11231@users.noreply.github.com>
2025-01-02 12:15:35 +0100

    fix(blog): Socials fixed

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,10 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/monoamine11231',
 		icon: GithubIcon
-	},
-
-	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
 	}
 ] as const
